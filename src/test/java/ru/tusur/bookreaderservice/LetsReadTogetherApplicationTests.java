d20cdc98842af71e6c5f8e7dca1eb7ea4e6c88b3
funnyIcecream <funnyicecream@funnyIceamsiMac.Dlink>
2023-08-19 10:22:12 +0300

    Initial commit

---
+++ src/test/java/ru/tusur/bookreaderservice/LetsReadTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tusur.bookreaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsReadTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
