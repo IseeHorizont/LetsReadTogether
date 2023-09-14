b98ba5bf851bc5a8e22e664b9f5d3c75e81dcbbc
funnyicecream <mak3.youself@gmail.com>
2023-09-14 20:24:31 +0300

    - some fix in security

--- src/main/java/ru/tusur/bookreaderservice/dto/RegisterRequest.java
+++ src/main/java/ru/tusur/bookreaderservice/dto/RegisterRequest.java
@@ -2,7 +2,6 @@ package ru.tusur.bookreaderservice.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,8 +16,7 @@ public class RegisterRequest {
     @NotBlank(message = "Имя не должно быть пустым")
     private String nickname;
 
-    @Email(message = "Не верный формат email-адреса", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
-    @NotEmpty(message = "email-адрес не должен быть пустым")
+    @Email(message = "Не верный формат email-адреса")
     private String email;
 
     @NotBlank(message = "Пароль не может быть пустым")
